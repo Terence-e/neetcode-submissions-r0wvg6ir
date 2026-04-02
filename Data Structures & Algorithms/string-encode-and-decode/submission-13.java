a63d9601e7a5040ad0474f86b118b61e455c0643
Terence-e <65685311+Terence-e@users.noreply.github.com>
2026-04-02 01:06:19 +0100

    Add: string-encode-and-decode - submission-13

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-13.java
@@ -0,0 +1,36 @@
+
+public class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder res = new StringBuilder();
+
+        for (String s : strs) {
+            res.append(s.length());
+            res.append('#');
+            res.append(s);
+        }
+
+        return res.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+
+        while (i < str.length()) {
+            int j = i;
+
+            while (str.charAt(j) != '#') {
+                j++;
+            }
+
+            int len = Integer.parseInt(str.substring(i, j));
+            j++;
+
+            res.add(str.substring(j, j + len));
+            i = j + len;
+        }
+
+        return res;
+    }
+}
